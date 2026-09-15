6ba5c0d29a482330db802e92cde05afaef45f569
r4nd0m4n0n-0 <xchan2180@gmail.com>
2026-09-16 00:47:03 +0800

    Clean up Swift package configuration

--- Package.swift
+++ Package.swift
@@ -1,18 +1,22 @@
 // swift-tools-version: 5.9
+
 import PackageDescription
 
 let package = Package(
-    name: "RetroPhosphor",
+    name: "LidWarp",
     platforms: [
         .macOS(.v13)
     ],
     products: [
-        .executable(name: "RetroPhosphor", targets: ["RetroPhosphor"])
+        .executable(
+            name: "RetroPhosphor",
+            targets: ["RetroPhosphor"]
+        )
     ],
     targets: [
         .executableTarget(
             name: "RetroPhosphor",
-            resources: [.process("Resources")]
+            path: "Sources/RetroPhosphor"
         )
     ]
 )
